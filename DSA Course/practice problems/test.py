58e814c486849fe3800e4456209bffff1a73a1dc
Nikhilgulati841 <nikhilgulati868720@gmail.com>
2024-12-26 17:53:28 +0530

    fdd scoring learning

--- DSA Course/practice problems/test.py
+++ DSA Course/practice problems/test.py
@@ -1,3 +1,23 @@
+def calculate_score(task_list,yes_no_score):
+    score=[]
+    print("\nChoose Yes | No\n")
+    for i in task_list:
+        while True:
+            try:
+                ask=input(f"Done with |{i}|- ").strip().lower()
+                if ask in ["yes","no"]:
+                    score.append(yes_no_score[i][ask.capitalize()])
+                    break
+                else:
+                    raise ValueError("Invalid option.")
+            except ValueError:
+                print("Wrong Option.\nChoose Yes | No\n")
+    return score
+
+def calculate_weightage(task_list,yes_no_score,score):
+    negative_total=0
+    for i in task_list:
+        # if i=="Time Waste" or "Anger" or "Maas" or "Hazard":  in this method, python finds all anger, maas etc as String and non-empty strings are considered as True in boolean context, so this will always print as if i=="Time Waste" or True:
 
 
 def main():
@@ -14,6 +34,11 @@ def main():
               "Anger":{"No":10,"Yes":-15},
               "Maas":{"No":10,"Yes":-7},
               "Hazard":{"No":8,"Yes":-10}}
+    # score=calculate_score(task_list,yes_no_score)
+    score=calculate_score(task_list,yes_no_score)
+    print("\n",sum(score))
+
+main()
     
 
 
